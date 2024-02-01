9ab9b79487ad02c1967d1e274a0537c0e3741f5a
Nordine Bittich <nordine@bittich.be>
2024-02-01 13:37:22 +0100

    setup frontend

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -4,26 +4,45 @@
 defmodule Dispatcher do
   use Matcher
   define_accept_types [
-    html: [ "text/html", "application/xhtml+html" ],
-    json: [ "application/json", "application/vnd.api+json" ]
+    html: ["text/html", "application/xhtml+html"],
+    json: ["application/json", "application/vnd.api+json"],
+    upload: ["multipart/form-data"],
+    sparql_json: ["application/sparql-results+json"],
+    any: [ "*/*" ],
   ]
 
-  @any %{}
-  @json %{ accept: %{ json: true } }
-  @html %{ accept: %{ html: true } }
+  define_layers [ :api_services, :api, :frontend, :not_found ]
 
-  define_layers [ :static, :services, :fall_back, :not_found ]
-
-  match "/bpmn-elements", @json do
+  match "/bpmn-elements",  %{ accept: [:json], layer: :api} do
     Proxy.forward conn, [], "http://resource/bpmn-elements"
   end
 
-  match "/bpmn-elements/*path", @json do
+  match "/bpmn-elements/*path", %{ accept: [:json], layer: :api}  do
     Proxy.forward conn, path, "http://resource/"
   end
 
-  match "/bpmn-files/*path", @any do
+  match "/bpmn-files/*path",  %{ accept: [:any], layer: :api} do
     Proxy.forward conn, path, "http://bpmn/"
+  end
+
+  ###############################################################
+  # frontend layer
+  ###############################################################
+
+  match "/assets/*path", %{ layer: :api } do
+    Proxy.forward conn, path, "http://frontend/assets/"
+  end
+
+  match "/@appuniversum/*path", %{ layer: :api } do
+    Proxy.forward conn, path, "http://frontend/@appuniversum/"
+  end
+
+  match "/*path", %{ accept: [:html], layer: :api } do
+    Proxy.forward conn, [], "http://frontend/index.html"
+  end
+
+  match "/*_path", %{ layer: :frontend } do
+    Proxy.forward conn, [], "http://frontend/index.html"
   end
 
   match "/*_", %{ layer: :not_found } do
