f5fac3a2d8fa92c32abd568b0623bcfac955186b
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-02-21 14:53:02 +0100

    Add caching

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,1 +1,16 @@
-export default [];
+export default [
+  {
+    match: {
+      subject: {},
+    },
+    callback: {
+      url: "http://resource/.mu/delta",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true,
+    },
+  },
+];
