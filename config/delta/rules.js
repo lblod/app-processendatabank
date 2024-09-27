48336d911afcb9165d1051323874793826697977
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-09-27 19:32:30 +0200

    Notify mu-search with deltas

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -13,4 +13,18 @@ export default [
       ignoreFromSelf: true,
     },
   },
+  {
+    match: {
+      // listen to all changes
+    },
+    callback: {
+      url: "http://search/update",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 10000,
+      ignoreFromSelf: true,
+    },
+  },
 ];
