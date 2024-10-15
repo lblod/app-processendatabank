f596914a7efde4d24f6ec4c2212691b748db8cf9
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-10-15 15:45:43 +0200

    Update reset elastic script

--- scripts/reset-elastic.sh
+++ scripts/reset-elastic.sh
@@ -2,7 +2,7 @@
 echo "warning this will run queries on the triplestore and delete containers, you have 3 seconds to press ctrl+c"
 sleep 3
 docker compose rm -fs elasticsearch search
-sudo rm -rf data/elasticsearch/
+find data/elasticsearch/ -mindepth 1 ! -name '.gitkeep' -delete
 docker compose exec -T virtuoso isql-v <<EOF
 SPARQL DELETE WHERE {   GRAPH <http://mu.semte.ch/authorization> {     ?s ?p ?o.   } };
 exec('checkpoint');
